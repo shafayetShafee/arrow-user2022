50d51add1eea4f136f7a72aae20c3b815135d33f
shafayetShafee <seaborn.sicario@gmail.com>
2022-07-04 18:53:25 +0600

    done with this amazing workshopgit status!

--- practicing_in_workshop.R
+++ practicing_in_workshop.R
@@ -416,7 +416,7 @@ parquet_file %>%
 toc()
 
 
-## part3: exercise
+## exercise part3 ----
 
 tic()
 nyc_taxi_2019_09 %>% write_csv_arrow("data/nyc_taxi_2019_09.csv")
@@ -448,4 +448,228 @@ read_parquet("data/nyc_taxi_2019_09.parquet", as_data_frame = FALSE)
 toc() # 0.04 sec
 
 
+## Multiile datasets -----
 
+nyc_taxi <- open_dataset("data/nyc-taxi-tiny/")
+
+tic()
+nyc_taxi %>%
+  filter(year == 2016, month == 9) %>%
+  nrow()
+toc()
+
+tic()
+nyc_taxi %>%
+  filter(pickup_location_id == 138) %>%
+  nrow()
+toc()
+
+
+## writing as parquet file with hive partitioning
+
+nyc_taxi_2016a <- open_dataset(
+  sources = "data/nyc-taxi-tiny/year=2016/",
+  format = "parquet",
+  unify_schemas = TRUE
+)
+
+
+tic()
+nyc_taxi_2016a %>%
+  group_by(month, vendor_name) %>%
+  write_dataset("data/nyc-taxi_2016")
+toc()
+
+nyc_taxi_2016b <- open_dataset("data/nyc-taxi_2016/")
+nyc_taxi_2016b
+
+
+tic()
+nyc_taxi_2016a %>%
+  filter(vendor_name == "CMT") %>%
+  group_by(month) %>%
+  summarize(distance = sum(trip_distance, na.rm = TRUE)) %>%
+  collect()
+toc()
+
+tic()
+nyc_taxi_2016b %>%
+  filter(vendor_name == "CMT") %>%
+  group_by(month) %>%
+  summarize(distance = sum(trip_distance, na.rm = TRUE)) %>%
+  collect()
+toc()
+
+
+## Exercises part3 ------
+
+# 1
+nyc_taxi_2019a <- open_dataset("data/nyc-taxi-tiny/year=2019") %>%
+  mutate(
+    monthday = mday(pickup_datetime),
+    yearday = yday(pickup_datetime)
+  )
+
+nyc_taxi_2019a %>%
+  select(pickup_datetime, monthday, yearday) %>%
+  collect()
+
+# 2
+nyc_taxi_2019a %>%
+  group_by(month, monthday) %>%
+  write_dataset("data/nyc-taxi_2019")
+
+nyc_taxi_2019a %>%
+  group_by(yearday) %>%
+  write_dataset("data/nyc-taxi_2019_yearday")
+
+
+# 3
+nyc_taxi_2019_monthday <- open_dataset("data/nyc-taxi_2019/")
+
+tic()
+nyc_taxi_2019_monthday %>%
+  filter(yearday %in% 81:90) %>%
+  group_by(yearday) %>%
+  summarise(
+    money_charged = sum(total_amount, na.rm = TRUE)
+  ) %>%
+  collect()
+toc()
+
+nyc_taxi_2019_yearday <- open_dataset("data/nyc-taxi_2019_yearday/")
+
+tic()
+nyc_taxi_2019_yearday %>%
+  filter(yearday %in% 81:90) %>%
+  group_by(yearday) %>%
+  summarise(
+    money_charged = sum(total_amount, na.rm = TRUE)
+  ) %>%
+  collect()
+toc()
+
+
+# part 4: Advanced Arrow --------------------------------------------------
+
+Scalar$create("new york")
+
+Array$create(c("new york", NA, NA, "philadelphia"))
+
+# R array is mutable but Arrow Array is immutable
+# Arrow Array is comprised of a fixed set of buffers-contiguous blocks
+# of memory dedicated to that array along with some metadata
+
+riots <- arrow_table(
+  location = chunked_array(
+    c("Inn", "cafe", "do-nuts", "deweys"),
+    c("Cross", "La Rambla")
+  ),
+  year = chunked_array(
+    c(1969, 1966, 1959, 1965),
+    c(1978, 1977)
+  ),
+  city = chunked_array(
+    c("new york", "san francisco", "los angles", "philadelphia"),
+    c("sydney", "barcelona")
+  )
+)
+
+class(riots)
+
+as.data.frame(riots) # returns tibble
+
+# Arrow Record Batch is just a collection of Arrow arrays
+# whereas, Arrow Table is a collection of Arrow chunked arrays
+
+
+# Big Picture -------------------------------------------------------------
+
+
+nyc_taxi <- open_dataset("data/nyc-taxi-tiny/")
+
+nyc_taxi_zones <- read_csv_arrow(
+  file = "data/taxi_zone_lookup.csv",
+  as_data_frame = FALSE,
+  skip = 1,
+  schema = schema(
+    LocationID = int64(),
+    Borough = utf8(),
+    Zone = utf8(),
+    service_zone = utf8()
+  )
+) %>%
+  rename(
+    location_id = LocationID,
+    borough = Borough,
+    zone = Zone,
+    service_zone = service_zone
+  )
+
+airport_zones <- nyc_taxi_zones %>%
+  filter(str_detect(zone, "Airport")) %>%
+  pull(location_id)
+
+dropoff_zones <- nyc_taxi_zones %>%
+  select(
+    dropoff_location_id = location_id,
+    dropoff_zone = zone
+  )
+
+airport_pickups <- nyc_taxi %>%
+  filter(pickup_location_id %in% airport_zones) %>%
+  select(
+    matches("datetime"),
+    matches("location_id")
+  ) %>%
+  left_join(dropoff_zones) %>%
+  count(dropoff_zone) %>%
+  arrange(desc(n)) %>%
+  collect()
+
+
+dat <- "data/taxi_zones/taxi_zones.shp" %>%
+  read_sf() %>%
+  clean_names() %>%
+  left_join(airport_pickups,
+            by = c("zone" = "dropoff_zone")) %>%
+  arrange(desc(n))
+
+windows()
+dat %>%
+  ggplot(aes(fill = n)) +
+  geom_sf(size = 0.1, color = "#222222") +
+  scale_fill_distiller(
+    name = "Number of trips",
+    labels = label_comma(),
+    palette = "Oranges",
+    direction = 1
+  ) +
+  geom_label_repel(
+    stat = "sf_coordinates",
+    data = dat %>%
+      mutate(
+        zone = case_when(
+          str_detect(zone, "Airport") ~ zone,
+          str_detect(zone, "Times") ~ zone,
+          TRUE ~ ""
+        )
+      ),
+    mapping = aes(label = zone, geometry = geometry),
+    max.overlaps = 50,
+    box.padding = 0.5,
+    label.padding = 0.5,
+    label.size = 0.15,
+    label.r = 0,
+    force = 30,
+    force_pull = 0,
+    fill = "white",
+    min.segment.length = 0
+  ) +
+  theme_void() +
+  theme(
+    text = element_text(color = "black"),
+    plot.background = element_rect(color = NA, fill = "#839496"),
+    legend.background = element_rect(fill = "white"),
+    legend.margin = margin(10, 10, 10, 10)
+  )
